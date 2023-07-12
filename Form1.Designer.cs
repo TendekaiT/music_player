da9b39973d9d5ecaf6e94c8423f649c2caf46352
TendekaiT <109837900+TendekaiT@users.noreply.github.com>
2023-07-12 21:09:30 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MusicPlayerApplication
+{
+    partial class MusicPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MusicPlayer));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxSongs = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.WindowsMusicPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WindowsMusicPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(580, 27);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(269, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Music Player App";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBoxSongs
+            // 
+            this.listBoxSongs.FormattingEnabled = true;
+            this.listBoxSongs.Location = new System.Drawing.Point(401, 33);
+            this.listBoxSongs.Name = "listBoxSongs";
+            this.listBoxSongs.Size = new System.Drawing.Size(155, 225);
+            this.listBoxSongs.TabIndex = 1;
+            this.listBoxSongs.SelectedIndexChanged += new System.EventHandler(this.listBoxSongs_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(401, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Choose Songs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(555, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(22, 20);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(176, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Developed by Tendekai Tembo";
+            // 
+            // WindowsMusicPlayer
+            // 
+            this.WindowsMusicPlayer.Enabled = true;
+            this.WindowsMusicPlayer.Location = new System.Drawing.Point(12, 33);
+            this.WindowsMusicPlayer.Name = "WindowsMusicPlayer";
+            this.WindowsMusicPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WindowsMusicPlayer.OcxState")));
+            this.WindowsMusicPlayer.Size = new System.Drawing.Size(358, 225);
+            this.WindowsMusicPlayer.TabIndex = 3;
+            // 
+            // MusicPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 300);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.WindowsMusicPlayer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBoxSongs);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MusicPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Player";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WindowsMusicPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxSongs;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private AxWMPLib.AxWindowsMediaPlayer WindowsMusicPlayer;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayerApplication
+{
+    public partial class MusicPlayer : Form
+    {
+        public MusicPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //select songs
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true; //code to select many songs
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files = ofd.SafeFileNames; //save track name in files array
+                paths = ofd.FileNames; //save file path in paths array
+
+                for(int i=0; i<files.Length; i++)
+                {
+                    listBoxSongs.Items.Add(files[i]); //show songs in listbox
+                }
+            }
+        }
+
+        //create array to save the title and path of music tracks
+
+        String[] paths, files;
+
+        private void listBoxSongs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //code will play selected song
+            WindowsMusicPlayer.URL = paths[listBoxSongs.SelectedIndex];
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //close the app
+            this.Close();
+        }
+    }
+}
